0d3706f71713fb15948833cf1c11cfc87576c6f7
Shaden14 <shadenabdullah14@gmail.com>
2024-10-21 09:44:44 +0300

    Update

---
+++ Recipe App/Recipe App/Recipe_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Recipe_AppApp.swift
+//  Recipe App
+//
+//  Created by Shaden Alhumood on 18/04/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct Recipe_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
