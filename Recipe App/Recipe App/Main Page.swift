c9c4e6cb8c548be3c3ff88d785789d56bc160073
Shaden14 <shadenabdullah14@gmail.com>
2024-10-22 10:13:23 +0300

    Main page

---
+++ Recipe App/Recipe App/Main Page.swift
@@ -0,0 +1,18 @@
+//
+//  Main Page.swift
+//  Recipe App
+//
+//  Created by Shaden Alhumood on 18/04/1446 AH.
+//
+
+import SwiftUI
+
+struct Main_Page: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Main_Page()
+}
