d15619699008a4ed9ee6291a57d86075072914cf
Shaden14 <shadenabdullah14@gmail.com>
2024-10-22 19:16:22 +0300

    New recipe page

---
+++ Recipe App/Recipe App/New Recipe.swift
@@ -0,0 +1,18 @@
+//
+//  New Recipe.swift
+//  Recipe App
+//
+//  Created by Shaden Alhumood on 19/04/1446 AH.
+//
+
+import SwiftUI
+
+struct New_Recipe: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    New_Recipe()
+}
