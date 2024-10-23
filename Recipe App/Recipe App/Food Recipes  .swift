a90c75a84d084d95008b5efcce2f0f0b0a15b36a
Shaden14 <shadenabdullah14@gmail.com>
2024-10-23 11:57:32 +0300

    Food Recipe page

---
+++ Recipe App/Recipe App/Food Recipes  .swift
@@ -0,0 +1,18 @@
+//
+//  Food Recipes  .swift
+//  Recipe App
+//
+//  Created by Shaden Alhumood on 19/04/1446 AH.
+//
+
+import SwiftUI
+
+struct Food_Recipes__: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Food_Recipes__()
+}
---
+++ Recipe App/Recipe App/learning.swift
@@ -0,0 +1,18 @@
+//
+//  learning.swift
+//  Recipe App
+//
+//  Created by Shaden Alhumood on 19/04/1446 AH.
+//
+
+import SwiftUI
+
+struct learning: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    learning()
+}
